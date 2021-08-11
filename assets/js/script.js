21a05bbe63a9d1aeb16784ad475b68a21719c7c1
Dilson Campêlo Rodrigues de Oliveira Neto <dilsono316@gmail.com>
2021-08-11 19:56:44 -0300

    Amém
    Fim do responsivo index

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,11 @@ function irTopo(){
     }
     )
 }
+function mudarMenu(){
+    let menu = document.querySelector(".lista_menu")
+    if(menu.style.display == "flex"){
+        menu.style.display = "none"
+    }else{
+        menu.style.display = "flex";
+    }
+}
