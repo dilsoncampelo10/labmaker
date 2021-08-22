3d76ea5e446454aa6018217bfc65eb995514bec2
dilsoncampelo10 <dilsono316@gmail.com>
2021-08-22 12:14:30 -0300

    Responsivo nota 10
    Deixando o novo layout responsivo

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,12 @@ function irTopo(){
 }
 function mudarMenu(){
     let menu = document.querySelector(".lista_menu")
+    let banner = document.querySelector('#banner')
     if(menu.style.display == "flex"){
         menu.style.display = "none"
+        banner.style.opacity = '100%'
     }else{
         menu.style.display = "flex";
+        banner.style.opacity = '5%'
     }
 }
